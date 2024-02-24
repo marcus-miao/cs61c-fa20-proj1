5797f1cb33a4e7303001cdd149488b42a64b4aea
marcus-miao <pmiao@andrew.cmu.edu>
2024-02-24 16:35:29 +0800

    pass steganography

--- steganography.c
+++ steganography.c
@@ -21,13 +21,52 @@
 //Determines what color the cell at the given row/col should be. This should not affect Image, and should allocate space for a new Color.
 Color *evaluateOnePixel(Image *image, int row, int col)
 {
-	//YOUR CODE HERE
+    Color *color = malloc(sizeof(Color));
+    uint8_t blue = image->image[row * image->cols + col]->B;
+
+    const uint8_t LSB_MASK = 0x01;
+    uint8_t white = blue & LSB_MASK;
+    if (white) {
+        color->R = 255;
+        color->G = 255;
+        color->B = 255;
+    } else {
+        color->R = 0;
+        color->G = 0;
+        color->B = 0;
+    }
+
+    return color;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
-	//YOUR CODE HERE
+    Image *imageDecoded = malloc(sizeof(Image));
+    imageDecoded->rows = image->rows;
+    imageDecoded->cols = image->cols;
+
+    uint64_t numPixels = image->rows * image->cols;
+    imageDecoded->image = malloc(sizeof(Color*) * numPixels);
+
+    for (uint32_t rowIdx = 0; rowIdx < image->rows; rowIdx++) {
+        for (uint32_t colIdx = 0; colIdx < image->cols; colIdx++) {
+            Color *color = evaluateOnePixel(image, rowIdx, colIdx);
+            imageDecoded->image[rowIdx * image->cols + colIdx] = color;
+        }
+    }
+
+    return imageDecoded;
+}
+
+void processCLI(int argc, char **argv, char **filename) 
+{
+	if (argc != 2) {
+		printf("usage: %s filename\n",argv[0]);
+		printf("filename is an ASCII PPM file (type P3) with maximum value 255.\n");
+		exit(-1);
+	}
+	*filename = argv[1];
 }
 
 /*
@@ -45,5 +84,13 @@ Make sure to free all memory before returning!
 */
 int main(int argc, char **argv)
 {
-	//YOUR CODE HERE
+    char *fileName;
+    processCLI(argc, argv, &fileName);
+
+    Image *image = readData(fileName);
+    Image *imageDecoded = steganography(image);
+    writeData(imageDecoded);
+
+    freeImage(image);
+    freeImage(imageDecoded);
 }
